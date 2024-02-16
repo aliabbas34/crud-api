cef67c45a7e423562da166e519efc25c2bfdff34
unknown <aliabbas7317@gmail.com>
2024-02-16 14:26:35 +0530

    user model generated, validation implemented, rspec spec for user model written

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with valid attributes" do
+    user=build(:user)
+    expect(user).to be_valid
+  end
+  it "is not valid if name is nil" do
+    user=build(:user,name:nil)
+    expect(user).not_to be_valid
+    expect(user.errors[:name]).to include("user name not present")
+  end
+  it "is not valid because name length is less than 3" do
+    user=build(:user,name:'aa')
+    expect(user).not_to be_valid
+    expect(user.errors[:name]).to include("length should be greater than or equal to 3")
+  end
+  it "is not valid if email is nil" do
+    user=build(:user,email:nil)
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to include("user email not present")
+  end
+  it "is not valid because email length is not between 10 to 255" do
+    user=build(:user,email:'aa')
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to include("email length should be between 10 to 255 characters")
+    user.email=('a'*255)+'@mail.com'
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to include("email length should be between 10 to 255 characters")
+  end
+  it "is not valid if paid_user is nil" do
+    user=build(:user,paid_user:nil)
+    expect(user).not_to be_valid
+  end
+end
